907bd4b26d6080559412a6b493bf70cc32df0e10
Guerric Chupin <guerric.chupin@gmail.com>
2016-05-20 17:25:28 +0100

    onChange function working

--- Reactogon/Auxiliary/Auxiliary.hs
+++ Reactogon/Auxiliary/Auxiliary.hs
@@ -2,15 +2,24 @@
 
 module Reactogon.Auxiliary.Auxiliary where
 
+import Data.Maybe
 import FRP.Yampa
 
-
+-- stepBack contains its previous argument as its output. Because it's
+-- hard to define it at time 0, it's wrapped up in a Maybe.
+stepBack :: SF a (Maybe a)
+stepBack = sscan f (Nothing, Nothing) >>^ snd
+  where f :: (Maybe a, Maybe a) -> a -> (Maybe a, Maybe a)
+        f (Nothing,Nothing) x' = (Just x', Nothing)
+        f (Just x, _) x' = (Just x', Just x)
 
 -- Throws an Event when the incoming signal change. The Event is
 -- tagged with the new value.
 onChange :: (Eq a) => SF a (Event a)
-onChange = proc a -> do
-  b <- onChange' -< a
-  returnA -< e
-  where onChange' :: (Eq a) => SF (a,a) Bool
-        onChange' = arr $ uncurry (==)
+onChange = proc x -> do
+  x' <- stepBack -< x
+  let makeEvent x x'
+        | isNothing x' = NoEvent
+        | isJust x' = let x'' = fromJust x' in
+            if x'' == x then NoEvent else Event x
+  returnA -< makeEvent x x'
