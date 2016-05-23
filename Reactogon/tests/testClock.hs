666be9436984a424b41565f46a1a3c4b3040f43d
Guerric Chupin <guerric.chupin@gmail.com>
2016-05-23 13:40:34 +0100

    Cleaned up testClock.

--- Reactogon/tests/testClock.hs
+++ Reactogon/tests/testClock.hs
@@ -4,11 +4,7 @@ import Reactogon.Semantics
 import FRP.Yampa
 
 main :: IO ()
-main = do{-
-  putStr "Test tempo: "
-  print testTempo'
-  putStr "Test onChange': "
-  print testonChange'-}
+main = do
   putStr "Testing metronome: "
   print testMetronome
 
@@ -19,9 +15,5 @@ tempo' = switch ((constant 30)
                                                &&&
                                                (after 20 60)) (constant))
 
-testTempo' = embed ((tempo'))
-  ((), take 120 $ repeat (1, Nothing))
-testonChange' = embed ((discard ^>> tempo' >>> onChange'))
-  ((), take 120 $ repeat (1, Nothing))
 testMetronome = embed (metronome (tempo'))
   ((), take 120 $ repeat (1, Nothing))
