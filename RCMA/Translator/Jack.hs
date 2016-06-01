ba667a43f30737b94df481a60f46c65a2f890ca0
Guerric Chupin <guerric.chupin@gmail.com>
2016-06-01 17:18:35 +0100

    Reworked on Jack. Doesn't compile.

--- RCMA/Translator/Jack.hs
+++ RCMA/Translator/Jack.hs
@@ -6,8 +6,9 @@ module RCMA.Translator.Jack where
 
 import qualified Control.Monad.Exception.Synchronous as Sync
 import qualified Control.Monad.Trans.Class           as Trans
+import qualified Data.EventList.Absolute.TimeBody    as EventListAbs
+import           Data.ReactiveValue
 import qualified Foreign.C.Error                     as E
-import           Hails.Yampa
 import           RCMA.Translator.Message
 import qualified Sound.JACK                          as Jack
 import qualified Sound.JACK.MIDI                     as JMIDI
@@ -21,6 +22,8 @@ inPortName = "input"
 outPortName :: String
 outPortName = "output"
 
+-- Starts a default client with an input and an output port. Doesn't
+-- do anything as such.
 jackSetup :: IO ()
 jackSetup = Jack.handleExceptions $
   Jack.withClientDefault rcmaName $ \client ->
@@ -28,9 +31,28 @@ jackSetup = Jack.handleExceptions $
   Jack.withPort client inPortName  $ \input ->
   jackRun client input output (jackCallBack client input output)
 
+-- Loop that does nothing except setting up a callback function
+-- (called when Jack is ready to take new inputs).
 jackRun client input output callback =
   Jack.withProcess client callback $ do
   Trans.lift $ putStrLn $ "Started " ++ rcmaName
   Trans.lift $ Jack.waitForBreak
 
-jackCallBack client input output = undefined
+-- The callback function. It pumps value out of the input port, mix
+-- them with value coming from the machine itself and stuff them into
+-- the output port. When this function is not running, events are
+-- processed.
+jackCallBack client input output nframes@(Jack.NFrames nframesInt) = do
+  -- This gets the sample rate of the client and the last frame number
+  -- it processed. We then use it to calculate the current absolute time
+  sr <- Trans.lift $ Jack.getSampleRate client
+  (Jack.NFrames lframeInt) <- Trans.lift $ Jack.lastFrameTime client
+  -- Read the events in the input buffer. /!\ This is in a weird
+  -- structure that we dismount right after into a list.
+  inBuffT <- JMIDI.readEventsFromPort input nframes
+  let rateD = fromIntegral sr
+      inBuff :: [(Frames, RawMessage)]
+      inBuff = map (\(Jack.NFrames n,e) -> (fromIntegral n, e)) $
+               EventListAbs.toPairList inBuffT
+
+  return ()
