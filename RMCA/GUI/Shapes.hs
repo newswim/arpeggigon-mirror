00c1925aceba090649b817cfa2beacf0b0e3a3f0
Guerric Chupin <guerric.chupin@gmail.com>
2016-06-16 15:54:50 +0100

    Hexagonal shape.

--- RMCA/GUI/Shapes.hs
+++ RMCA/GUI/Shapes.hs
@@ -7,9 +7,32 @@ import Graphics.Rendering.Cairo
 -- Colors are given in RGB format, coefficient goes from 0 to 1
 hexagon :: (Double, Double, Double) -- Background color
         -> (Double, Double, Double) -- Frame color
+        -> (Double, Double) -- Center
         -> Double -- Width
         -> Render ()
-hexagon (backR, backG, backB) (frameR, frameG, frameB) w = do
+hexagon (backR, backG, backB) (frameR, frameG, frameB) (x,y) w = do
   setSourceRGB frameR frameG frameB
-  setLineWidth (0.1 * w)
+  setLineWidth (0.05 * w)
+
+  let a = 0.5*w
+      b = 0.87*w
+
+  moveTo (x+a) (y-b)
+  lineTo (x-a) (y-b)
+  lineTo (x-w) y
+  lineTo (x-a) (y+b)
+  lineTo (x+a) (y+b)
+  lineTo (x+w) y
+  closePath
+  strokePreserve
+  setSourceRGB backR backG backB
+  fill
   return ()
+
+pnw = 200
+pnh = 200
+
+main = withImageSurface FormatARGB32 pnw pnh
+  (\srf -> do renderWith srf (hexagon (1,1,1) (0,0,0)
+                              (fromIntegral pnw/2,fromIntegral pnh/ 2) (fromIntegral (pnw `quot` 2)))
+              surfaceWriteToPNG srf "myDraw.png")
